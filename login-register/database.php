7a21d31ce1b68ca3fd48550ca8044093ab7dce13
Smart Picxel <135949405+smartpicxel@users.noreply.github.com>
2023-12-25 16:33:32 +0530

    Update database.php

--- login-register/database.php
+++ login-register/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostName = "localhost";
-$dbUser = "root";
-$dbPassword = "";
-$dbName = "login_register";
+$dbUser = "id21707313_root";
+$dbPassword = "Loginroot@1234";
+$dbName = "id21707313_users";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
